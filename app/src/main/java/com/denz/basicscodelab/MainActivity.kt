aebb6b7e970bf69d082c315076bb1a72c080d9ee
Denzel313 <maple4064@gmail.com>
2023-12-14 13:20:13 +0700

    ubah warna

--- app/src/main/java/com/denz/basicscodelab/MainActivity.kt
+++ app/src/main/java/com/denz/basicscodelab/MainActivity.kt
@@ -10,7 +10,6 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -30,7 +29,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.denz.basicscodelab.ui.theme.BasicsCodelabTheme
-import com.denz.basicscodelab.ui.theme.Typography
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
